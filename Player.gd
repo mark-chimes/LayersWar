97984fca4558fee49f6997b85d6bdf26a44ec3c5
Mark Chimes <chimes.mark@gmail.com>
2020-11-13 15:34:57 +0200

    Fall off the edges.

--- Player.gd
+++ Player.gd
@@ -1,51 +1,37 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-#var h_speed = 50
-#var v_speed = 0
-#var v_acc = 200
-#var v_terminal = 1000
+var v_speed = 0
+var v_acc = 200
+var v_terminal = 1000
 
 export (int) var speed = 200
 var velocity = Vector2()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity = Vector2()
-	if Input.is_action_pressed('walk_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('walk_left'):
-		velocity.x -= 1
-#	if Input.is_action_pressed('walk_down'):
-#		velocity.y += 1
-#	if Input.is_action_pressed('walk_up'):
-#		velocity.y -= 1
-	velocity = velocity.normalized() * speed
-	
-	if velocity.x < -1: 
-		$AnimatedSprite.flip_h = true
-	elif velocity.x > 1: 
-		$AnimatedSprite.flip_h = false
+	if position.x > 0 and position.x < 1024: 
+		velocity = Vector2()
+		if Input.is_action_pressed('walk_right'):
+			velocity.x += 1
+		if Input.is_action_pressed('walk_left'):
+			velocity.x -= 1
+		velocity = velocity.normalized() * speed
+		
+		if velocity.x < -1: 
+			$AnimatedSprite.flip_h = true
+		elif velocity.x > 1: 
+			$AnimatedSprite.flip_h = false
 
-	if velocity.length() > 0: 
-		$AnimatedSprite.playing = true
-	else: 
-		$AnimatedSprite.playing = false
-
-	
-	position = position + velocity*delta
-
-#	if position.x < 1024: 
-#		position = position + Vector2(delta * h_speed, 0)
-#	else: 
-#		if v_speed < v_terminal: 
-#			v_speed += delta*v_acc
-#		position = position + Vector2(delta * h_speed, delta * v_speed)
+		if velocity.length() > 0: 
+			$AnimatedSprite.playing = true
+		else: 
+			$AnimatedSprite.playing = false
+	else:
+		if v_speed < v_terminal: 
+			v_speed += delta*v_acc
+		velocity.y += delta * v_speed
+					
+	position = position + velocity*delta	
