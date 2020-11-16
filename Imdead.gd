f712af763193e008b0571d8a955c428f5cec92f4
Mark Chimes <chimes.mark@gmail.com>
2020-11-16 21:52:18 +0200

    Knight kills skeleton

--- Imdead.gd
+++ Imdead.gd
@@ -2,8 +2,28 @@ extends Node2D
 
 export var velocity = 10
 
+enum State {
+	WALK,
+	ATTACK,
+	DIE
+}
+
+var state = State.WALK
+
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	position.x = position.x + velocity*delta	
+	if state == State.WALK:
+		position.x = position.x + velocity*delta	
+
+func _on_Area2D_area_entered(area):
+	$AnimatedSprite.play("attack")
+	state = State.ATTACK
+
+func _on_AnimatedSprite_animation_finished():
+	if state == State.ATTACK:
+		$AnimatedSprite.play("death")
+		state = State.DIE
+	elif state == State.DIE:
+		queue_free()
--- Knight.gd
+++ Knight.gd
@@ -1,9 +1,30 @@
 extends Node2D
 
 export var velocity = 10
+var cur_velocity
+
+enum State {
+	RUN,
+	ATTACK,
+	DIE,
+	IDLE
+}
+
+var state = State.RUN
 
 func _ready():
-	pass # Replace with function body.
-
+	cur_velocity = velocity
+	$AnimatedSprite.play("walk")
+	
 func _process(delta):
-	position.x = position.x + velocity*delta	
+	if state == State.RUN:
+		position.x = position.x + cur_velocity*delta	
+
+func _on_Area2D_area_entered(area):
+	$AnimatedSprite.play("attack")
+	state = State.ATTACK
+
+func _on_AnimatedSprite_animation_finished():
+	if state == State.ATTACK:
+		$AnimatedSprite.play("idle")
+		state = State.IDLE
